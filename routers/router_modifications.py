71e632218491d0b8eba76e53a64df2814e68cbd9
Jorge <jorgenathanael53@gmail.com>
2025-07-26 21:55:36 -0300

    refactor: renaming function

--- routers/router_modifications.py
+++ routers/router_modifications.py
@@ -4,6 +4,6 @@ from controllers.modify_controllers import mofify_status
 router = APIRouter()
 
 @router.patch("/status/{email}")
-async def edit_status(email):
+async def activate_user(email):
     print(f"rota| edit_status: {email}")
     return await mofify_status(str(email))
