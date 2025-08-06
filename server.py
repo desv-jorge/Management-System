6e60645aecd70f773ebc92ffdb59bf7789d4521e
Nathanael Jorge <jorgenathanael53@gmail.com>
2025-08-05 23:53:22 -0300

    cleanup: organizing the project

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
