d0ce95801d9a39cbcdd0c56db32cf81aba566c7b
Nathanael Jorge <jorgenathanael53@gmail.com>
2025-08-07 02:48:37 -0300

    refactor: implementing a new logo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 import { FaEdit, FaTrashAlt, FaPlus } from "react-icons/fa";
-import { RiAccountCircleFill } from "react-icons/ri";
-import logo from "../assets/logo.jpg";
-import "../style.css";
+
+import Header from "../components/header";
 
 const services = [
   {
@@ -45,21 +44,7 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen bg-gray-200 font-sans">
       {/* Navbar */}
-      <header className="bg-black text-white flex justify-between items-center px-6 py-4">
-        <div className="flex items-center gap-2">
-          <img src={logo} alt="+ Celulares" className="size-12 rounded-full" />
-        </div>
-        <nav className="flex gap-8 text-lg">
-          <a href="#" className="underline">Início</a>
-          <a href="#">Estoque</a>
-          <a href="#">Serviços</a>
-          <a href="#">Clientes</a>
-          <a href="#">Vendas</a>
-        </nav>
-        <div className="text-2xl">
-            <RiAccountCircleFill className="size-12"/>
-        </div>
-      </header>
+      <Header/>
 
       {/* Conteúdo principal */}
       <main className="p-8">
